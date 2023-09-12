7369314ee71b49480baf0a2c9775befb9b2e61b0
mmerga <78653901+mmerga@users.noreply.github.com>
2023-09-12 02:59:53 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,60 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  test('convert valid input', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert?input=10L')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '{"initNum":10,"initUnit":"L","returnNum":2.64172,"returnUnit":"gal","string":"10 liters converts to 2.64172 gallons"}');
+        done();
+      });
+  });
+  test('convert invalid unit', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert?input=30g')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '"invalid unit"');
+        done();
+      });
 
+  });
+  test('convert invalid number', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert?input=3/7.2/4kg')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '"invalid number"');
+        done();
+      });
+
+  });
+  test('convert invalid number and unit', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert?input=3/7.2/4kilomegagram')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '"invalid number and unit"');
+        done();
+      });
+
+  });
+  test('convert no number', function(done) {
+    chai
+      .request(server)
+      .get('/api/convert?input=kg')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '{"initNum":1,"initUnit":"kg","returnNum":2.20462,"returnUnit":"lbs","string":"1 kilograms converts to 2.20462 pounds"}');
+        done();
+      });
+  });
+  after(function() {
+        chai.request(server).get('/api')
+    });
 });
